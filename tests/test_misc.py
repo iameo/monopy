8a3b7e46df73bc3f822ef171faaccbd390f55f3d
iameo <okwudike.emmanuel@gmail.com>
2021-08-20 13:38:30 +0100

    test the misc endpoint - 100% coverage

---
+++ tests/test_misc.py
@@ -0,0 +1,40 @@
+import os
+import pytest
+import random
+
+from mono.api.misc import Misc
+from mono.api.errors import MonoAuthException
+
+TEST_MONO_SEC_KEY = 'test_sk_xxxxxxxxxxxxx'
+
+
+@pytest.fixture(scope='module')
+def mono_instance():
+    '''
+    mono_instance to handle calls
+    '''
+    misc = Misc(TEST_MONO_SEC_KEY)
+    return misc
+
+
+def test_initialization(mono_instance):
+    '''
+    assert that mono_instance is not None
+    '''
+    assert mono_instance is not None
+
+
+def test_initialization_exception_no_parameters():
+    '''
+    raise MonoAuthException when mono_sec_key is set to None
+    '''
+    with pytest.raises(MonoAuthException):
+        Misc(None)
+
+
+def test_institutions(mono_instance):
+    '''
+    test the institutions endpoint to fetch available institutions on MONO
+    '''
+    status, response = mono_instance.institutions()
+    assert status and len(response) >= 1 #at least one or more info present
