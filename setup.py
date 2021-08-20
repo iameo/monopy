62cf900ec3557b81e4dcb869d20c4410314895d0
iameo <okwudike.emmanuel@gmail.com>
2021-08-20 15:26:24 +0100

    updated version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="monopy",
-    version="0.0.1",
+    version="1.0.0",
     author="Emmanuel Okwudike",
     author_email="okwudike.emmanuel@gmail.com",
     description="a",
