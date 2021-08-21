f5fc6f85decab446cc62f629dd207173f2a5b13c
iameo <okwudike.emmanuel@gmail.com>
2021-08-21 13:59:39 +0100

    version uppity

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="monopy",
-    version="1.0.0",
+    version="1.0.1",
     author="Emmanuel Okwudike",
     author_email="okwudike.emmanuel@gmail.com",
     description="a python wrapper for the Mono API",
