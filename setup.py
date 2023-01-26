8d3b7045aea0a7c59e78ad0316b0ae3277d9e738
PPE <okwudike.emmanuel@gmail.com>
2023-01-26 02:18:22 +0100

    minor version bump

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="monopy",
-    version="1.0.3",
+    version="1.0.4",
     author="Emmanuel Okwudike",
     author_email="okwudike.emmanuel@gmail.com",
     description="a python wrapper for the Mono API",
